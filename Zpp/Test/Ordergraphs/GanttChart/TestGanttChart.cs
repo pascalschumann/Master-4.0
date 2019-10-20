d908d971faea2fdf8db6db69887cae22844173ac
pascalschumann <18259965+pascalschumann@users.noreply.github.com>
2019-10-20 12:16:58 +0200

    minor

--- Zpp/Test/Ordergraphs/GanttChart/TestGanttChart.cs
+++ Zpp/Test/Ordergraphs/GanttChart/TestGanttChart.cs
@@ -20,6 +20,7 @@ namespace Zpp.Test.Ordergraphs.GanttChart
 
         [Theory]
         [InlineData(TestConfigurationFileNames.DESK_COP_5_LOTSIZE_2)]
+        [InlineData(TestConfigurationFileNames.DESK_COP_2_LOTSIZE_2)]
         [InlineData(TestConfigurationFileNames.TRUCK_COP_5_LOTSIZE_2)]
         [InlineData(TestConfigurationFileNames.TRUCK_COP_1_LOTSIZE_1)]
         public void TestGanttChartBar(string testConfigurationFileName)

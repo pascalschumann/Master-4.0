120d38d66d06f673fea258a3801d42701aae4088
pascalschumann <18259965+pascalschumann@users.noreply.github.com>
2019-11-19 09:38:55 +0100

    adapt performance study parameters

--- Zpp/Test/Configuration/TestConfigurationFileNames.cs
+++ Zpp/Test/Configuration/TestConfigurationFileNames.cs
@@ -15,6 +15,9 @@ namespace Zpp.Test.Configuration
         public const string DESK_COP_100_LOTSIZE_2 = _basePath + "tisch_cop_100_lotsize_2.json";
         public const string DESK_COP_2_LOTSIZE_2 =
             _basePath + "tisch_cop_2_lotsize_2.json";
+        
+        public const string DESK_INTERVAL_20160_COP_100_LOTSIZE_2 = 
+            _basePath + "desk_interval_20160_cop_100_lotsize_2.json";
 
         // truck
         public const string TRUCK_COP_5_LOTSIZE_2 = _basePath + "truck_cop_5_lotsize_2.json";
--- Zpp/Test/Integration_Tests/TestPerformance.cs
+++ Zpp/Test/Integration_Tests/TestPerformance.cs
@@ -44,8 +44,8 @@ namespace Zpp.Test.Integration_Tests
         }
 
         [Theory]
-        [InlineData(TestConfigurationFileNames.TRUCK_COP_5_LOTSIZE_2)]
         [InlineData(TestConfigurationFileNames.DESK_COP_100_LOTSIZE_2)]
+        [InlineData(TestConfigurationFileNames.DESK_INTERVAL_20160_COP_100_LOTSIZE_2)]
         [InlineData(TestConfigurationFileNames.TRUCK_COP_100_LOTSIZE_2)]
         [InlineData(TestConfigurationFileNames.TRUCK_INTERVAL_20160_COP_100_LOTSIZE_2)]
         public void TestPerformanceStudyWithoutDbPersist(string testConfigurationFileName)
